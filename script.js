d2935aaf0d20897b534a98150e81d5add39f2eea
Elizabeth <ek@elizabethkillgore.com>
2023-06-12 18:08:15 -0500

    add script.js, connect to index.html corrected

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript. It is written, greater is He Who is in me that he who is in the world." ;
+h2.textContent = "This content added by JavaScript. It is written, greater is He Who is in me than he who is in the world." ;
 document.querySelector("body").appendChild(h2);
 
